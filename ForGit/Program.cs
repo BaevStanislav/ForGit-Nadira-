9517411e92c86f2b46ebadf0939382c64fc828de
Stanislav Baev <baev.stas@mail.ru>
2022-08-08 20:29:59 +0300

    +Piramide

--- ForGit/Program.cs
+++ ForGit/Program.cs
@@ -10,6 +10,30 @@ namespace ForGit
     {
         static void Main(string[] args)
         {
-        }
+
+            Console.WriteLine("Hello Gitttt");
+
+            int n = 5, i, j, num = 1, gap;
+            gap = n - 1;
+            for (j = 1; j <= n; j++)
+            {
+                num = j;
+                for (i = 1; i <= gap; i++)
+                    Console.Write("");
+                gap--;
+                for (i = 1; i <= j; i++)
+                {
+                    Console.Write(num);
+                    num++;
+                }
+                num--;
+                num--;
+                for (i = 1; i < j; i++)
+                {
+                    Console.Write(num);
+                    num--;
+                }
+                Console.WriteLine("");
+            }
     }
 }
